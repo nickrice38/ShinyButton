0e8ec0f91b32904bbab009ed6b0195703973a244
Nick Rice <nick.rice@starlingbank.com>
2023-03-03 23:06:43 +0000

    Initial Commit

---
+++ ButtonExperiments/ButtonExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonExperimentsApp.swift
+//  ButtonExperiments
+//
+//  Created by Nick Rice on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
